3c94699aba772fb53ccdb691fcfeb3bea781d005
afonso mateus <afonsoprad10@gmail.com>
2021-07-28 12:16:11 -0400

    setup project

---
+++ src/test/java/br/com/poc/sqs/SqsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.poc.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
