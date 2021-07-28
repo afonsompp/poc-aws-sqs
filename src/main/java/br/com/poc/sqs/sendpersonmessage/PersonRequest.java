f4f7f0fcb0a6fc280fc961be0790150512f2a3c5
afonso mateus <afonsoprad10@gmail.com>
2021-07-28 16:50:37 -0400

    dto to recive requests

---
+++ src/main/java/br/com/poc/sqs/sendpersonmessage/PersonRequest.java
@@ -0,0 +1,52 @@
+package br.com.poc.sqs.sendpersonmessage;
+
+import java.time.LocalDate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hibernate.validator.constraints.br.CPF;
+
+public class PersonRequest {
+
+	@NotBlank
+	@Size(min = 3)
+	private String firstName;
+	@NotBlank
+	@Size(min = 3)
+	private String lastName;
+	@NotBlank
+	@CPF
+	private String cpf;
+	@NotNull
+	@Past
+	private LocalDate bornDate;
+
+	@Deprecated
+	public PersonRequest() {}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public String getCpf() {
+		return this.cpf;
+	}
+
+	public LocalDate getBornDate() {
+		return this.bornDate;
+	}
+
+	public String toJsonString() throws JsonProcessingException {
+		var mapper = new ObjectMapper();
+
+		return mapper.writeValueAsString(this);
+	}
+
+}
