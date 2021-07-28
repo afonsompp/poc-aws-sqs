a913c6399dd59e931c9ed0ce224f73941710602f
afonso mateus <afonsoprad10@gmail.com>
2021-07-28 19:28:55 -0400

    calling message sender in controller

--- src/main/java/br/com/poc/sqs/sendpersonmessage/PersonController.java
+++ src/main/java/br/com/poc/sqs/sendpersonmessage/PersonController.java
@@ -1,6 +1,7 @@
 package br.com.poc.sqs.sendpersonmessage;
 
 import javax.validation.Valid;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/person")
 public class PersonController {
 
+	private final SendSqsMessageService sendSqsMessage;
+
+	public PersonController(SendSqsMessageService sendSqsMessage) {
+		this.sendSqsMessage = sendSqsMessage;
+	}
+
 	@PostMapping
 	public ResponseEntity<PersonResponse> sendSqsMessage(
-			@RequestBody @Valid PersonRequest personRequest) {
+			@RequestBody @Valid PersonRequest personRequest) throws JsonProcessingException {
 
+		sendSqsMessage.send(personRequest.toJsonString());
 		return ResponseEntity.ok(new PersonResponse("Person submitted with success"));
 	}
 
