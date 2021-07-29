a893d748f697673a0049c997cbea00ab23dd2292
afonso mateus <afonsoprad10@gmail.com>
2021-07-28 20:07:05 -0400

    create person parser

---
+++ src/main/java/br/com/poc/sqs/savepersoninformation/PersonParser.java
@@ -0,0 +1,22 @@
+package br.com.poc.sqs.savepersoninformation;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class PersonParser {
+
+	private PersonParser() {}
+
+	public static Person convertToPerson(String personString) {
+		var mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+
+		try {
+			return mapper.readValue(personString, Person.class);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException("parameter cannot be parsed in Person class", e);
+
+		}
+	}
+}
