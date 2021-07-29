cf394fbc5c751744775e361eb116d1b2bbf64bb6
afonso mateus <afonsoprad10@gmail.com>
2021-07-28 20:11:01 -0400

    calling person service method to handle message

--- src/main/java/br/com/poc/sqs/recivepersonmessage/PersonMessageListener.java
+++ src/main/java/br/com/poc/sqs/recivepersonmessage/PersonMessageListener.java
@@ -1,13 +1,21 @@
 package br.com.poc.sqs.recivepersonmessage;
 
 import org.springframework.stereotype.Service;
+import br.com.poc.sqs.savepersoninformation.PersonService;
 import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
 import io.awspring.cloud.messaging.listener.annotation.SqsListener;
 
 @Service
 public class PersonMessageListener {
+
+	private final PersonService personService;
+
+	public PersonMessageListener(PersonService personService) {
+		this.personService = personService;
+	}
+
 	@SqsListener(value = "person-queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-	public void receive(String message) {
-		System.out.println(message);
+	public void receivePersonMessage(String personString) {
+		personService.savePerson(personString);
 	}
 }
